7b4a973daa1b214c35dfb578d88c981cb6761fd2
pushpendra8081 <pushpendrapal8081@gmail.com>
2023-11-14 20:58:05 +0530

    Calculator App

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView resultTv,solutionTv;
+    MaterialButton buttonC,buttonBackOpen,buttonBackclose;
+    MaterialButton buttonDivid,buttonMultiply,buttonMinus,buttonPlus,buttonEquals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    MaterialButton buttonAC,buttonDot;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultTv  =findViewById(R.id.result_tv);
+        solutionTv=findViewById(R.id.solution_tv);
+
+        assignId(buttonC,R.id.button_c);
+        assignId(buttonBackOpen,R.id.button_open_bracket);
+        assignId(buttonBackclose,R.id.button_close_bracket);
+        assignId(buttonDivid,R.id.button_divid);
+        assignId(buttonMultiply,R.id.button_multiply);
+        assignId(buttonMinus,R.id.button_minus);
+        assignId(buttonPlus,R.id.button_plus);
+        assignId(buttonEquals,R.id.button_equals);
+        assignId(button0,R.id.button_0);
+        assignId(button1,R.id.button_1);
+        assignId(button2,R.id.button_2);
+        assignId(button3,R.id.button_3);
+        assignId(button4,R.id.button_4);
+        assignId(button5,R.id.button_5);
+        assignId(button6,R.id.button_6);
+        assignId(button7,R.id.button_7);
+        assignId(button8,R.id.button_8);
+        assignId(button9,R.id.button_9);
+        assignId(buttonAC,R.id.button_ac);
+        assignId(buttonDot,R.id.button_dot);
+
+
+    }
+
+    void assignId(MaterialButton btn,int id){
+        btn=findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if (buttonText.equals("AC")) {
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        } else if (buttonText.equals("=")) {
+            solutionTv.setText(resultTv.getText());
+        } else if (buttonText.equals("C")) {
+            if (dataToCalculate.length() > 0) {
+                dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+            }
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if (!finalResult.equals("err")) {
+            resultTv.setText(finalResult);
+        }
+    }
+
+    String getResult(String data) {
+        try {
+            // Add a check to handle empty input
+            if (data.isEmpty()) {
+                return "0";
+            }
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initSafeStandardObjects();
+
+            // Append a ';' at the end to ensure that incomplete expressions are evaluated
+            String finalResult = context.evaluateString(scriptable, data + ";", "javscript", 1, null).toString();
+
+            if (finalResult.endsWith(".0")) {
+                finalResult = finalResult.replace(".0", "");
+            }
+            return finalResult;
+        } catch (Exception e) {
+            return "err";
+        }
+    }
+
+}
